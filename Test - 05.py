7b935eb5532b0a8a04e1655b4fa21ce1b2deee25
Sarfall <leorlara@outlook.com>
2025-01-12 22:40:09 +0200

    Added a loop

--- Test - 05.py
+++ Test - 05.py
@@ -1,40 +1,42 @@
-code = input("Enter the room code:").title()
+while True:  # Infinite loop to keep asking for requests
+    code = input("Enter the room code (or type 'exit' to quit): ").title()
 
-rooms = {
-    "Main Reception": "at the main entrance of the A block",
-    "Staffroom": "on the top floor of the B block",
-    "Sports Hall": "on the side of the B Block",
-    "Dance Studio": "on the ground floor in the A Block",
-    "Activity Studio": "on the ground floor in the A Block"
-}
+    if code == 'Exit':
+        print("Exiting the program...")
+        break  # Exit the loop if the user types 'exit'
 
-# Check if the code is valid (either a room code or a room name)
-while len(code) > 4 or not code[0].isalpha():
-    if code not in rooms:  # Invalid input not in rooms dictionary
-        print("Room number is invalid. Please enter a valid room number.")
-        code = input("Enter the room code:").title()
+    # Process the code as needed
+    rooms = {
+        "Main Reception": "at the main entrance of the A block",
+        "Staffroom": "on the top floor of the B block",
+        "Sports Hall": "on the side of the B Block",
+        "Dance Studio": "on the ground floor in the A Block",
+        "Activity Studio": "on the ground floor in the A Block"
+    }
+
+    while len(code) > 4 or not code[0].isalpha():
+        if code not in rooms:
+            print("Room number is invalid. Please enter a valid room number.")
+            code = input("Enter the room code:").title()
+        else:
+            break
+
+    # Process the floor number or the room name as described earlier
+    if code[0].isalpha() and code[1:].isdigit():
+        Floor = int(code[1:])
+        if code[0] == "S":
+            print("Room is in the 6th form block.")
+        else:
+            print("Room is in the", code[0], "block.")
+        
+        if Floor >= 0 and Floor < 100:
+            print("Room is on the ground floor.")
+        elif Floor >= 100 and Floor < 200:
+            print("Room is on the first floor.")
+        elif Floor >= 200:
+            print("Room is on the second floor.")
+
+    elif code in rooms:
+        print("This room is located " + rooms[code])
     else:
-        break  # Room name found in the dictionary, exit loop
-
-# If the code is a room number (i.e., it has a number after the first character)
-if code[0].isalpha() and code[1:].isdigit():
-    Floor = int(code[1:])  # Extract floor number from the code (e.g., "A203" -> 203)
-
-    if code[0] == "S":
-        print("Room is in the 6th form block.")
-    else:
-        print("Room is in the", code[0], "block.")
-
-    if Floor >= 0 and Floor < 100:
-        print("Room is on the ground floor.")
-    elif Floor >= 100 and Floor < 200:
-        print("Room is on the first floor.")
-    elif Floor >= 200:
-        print("Room is on the second floor.")
-
-# If the code is a room name from the dictionary
-elif code in rooms:
-    print("This room is located " + rooms[code])
-
-else:
-    print("We cannot locate this room! Are you sure this is a valid room?")
+        print("We cannot locate this room! Are you sure this is a valid room?")
