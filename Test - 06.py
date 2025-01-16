4dc7cb905be8beb3c612c0a0efaefca1213b9cae
Sarfall <leorlara@outlook.com>
2025-01-16 21:54:17 +0200

    Created the function

--- Test - 06.py
+++ Test - 06.py
@@ -1,1 +1,8 @@
-#test
+def triangle_area (a,b,c):
+    s = (a+b+c)/2
+    area = (s*(s-a)*(s-b)*(s-c))**0.5
+    return area
+
+input_a = float(input("Enter the length of side 1: "))
+input_b = float(input("Enter the length of side 2: "))
+input_c = float(input("Enter the length of side c3 "))
