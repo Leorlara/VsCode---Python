91978629c7d44e970237065dfe1705267d0d7f55
Sarfall <leorlara@outlook.com>
2025-01-16 22:01:16 +0200

    Added more fuctions for different shapes

--- Test - 06.py
+++ Test - 06.py
@@ -3,6 +3,14 @@ def triangle_area (a,b,c):
     area = (s*(s-a)*(s-b)*(s-c))**0.5
     return area
 
+def square_area (a):
+    return a**2
+
+def circle_area (r):
+    return 3.14159 * r**2
+
 input_a = float(input("Enter the length of side 1: "))
 input_b = float(input("Enter the length of side 2: "))
-input_c = float(input("Enter the length of side c3 "))
+input_c = float(input("Enter the length of side 3: "))
+
+print(f"The area of the triangle is: {triangle_area(input_a, input_b, input_c):.2f}")
